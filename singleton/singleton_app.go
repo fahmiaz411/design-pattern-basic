2414beeedf8a60c41fe329a9dc4a7b97aec8f24e
fahmiaz411 <fahmiaziz884@gmail.com>
2022-05-31 10:02:10 +0700

    sample save

--- singleton/singleton_app.go
+++ singleton/singleton_app.go
@@ -1,5 +1,9 @@
 package singleton
 
 func init() {
+	orderService := new(OrderService)
+	orderService.Save("0001")
 
+	orderDetailService := new(OrderDetailService)
+	orderDetailService.Save("0001", "Indomie")
 }
