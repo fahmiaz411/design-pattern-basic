a2461587cce6f4ed28f7dab309e0372ec2093988
fahmiaz411 <fahmiaziz884@gmail.com>
2022-05-31 10:08:17 +0700

    create all in one connection

--- singleton/singleton_app.go
+++ singleton/singleton_app.go
@@ -1,9 +1,14 @@
 package singleton
 
+// Singleton will use the same object, in sample
+// This singleton use 1 connection to handle all
+// of its query
 func init() {
-	orderService := new(OrderService)
+	db := connect("localhost", "root", "1234")
+
+	orderService := &OrderService{DB: db}
 	orderService.Save("0001")
 
-	orderDetailService := new(OrderDetailService)
+	orderDetailService := &OrderDetailService{DB: db}
 	orderDetailService.Save("0001", "Indomie")
 }
